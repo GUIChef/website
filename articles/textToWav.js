7bd30b142043f342a7a1bb7f3be467ed3a532f2d
GUIChef <Professionalrandy@outlook.com>
2026-08-10 14:34:53 -0700

    Creo

---
+++ articles/textToWav.js
@@ -0,0 +1,8 @@
+function textToWav() {
+  const reader = new FileReader();
+  reader.readAsText(file);
+  reader.addEventListener('load', (penisPower) => {
+    const data = penisPower.target.result;
+    console.log(data);
+  })
+}
