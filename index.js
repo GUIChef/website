3518c5e3e5ecb3a23f8d70aaf9aaf2851ab91e83
GUIChef <Professionalrandy@outlook.com>
2026-08-09 20:06:43 -0700

    Creo

---
+++ index.js
@@ -0,0 +1,14 @@
+function microwaveOpen(){
+  document.getElementById("microwave").src="images/microwaveOpen.png"
+  document.getElementById("microwave").title="This is an open microwave."
+  document.getElementById("microwave").alt="This is an open microwave."
+  document.getElementById("microwave").id="microwaveOpen"
+  document.getElementById("microwaveButton").onclick=microwaveClose
+}
+function microwaveClose(){
+	document.getElementById("microwaveOpen").src="images/microwave.png"
+  document.getElementById("microwaveOpen").title="This is a microwave."
+  document.getElementById("microwaveOpen").alt="This is a microwave."
+  document.getElementById("microwaveOpen").id="microwave"
+  document.getElementById("microwaveButton").onclick=microwaveOpen
+}
