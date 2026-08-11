fbf2981255b431341179787bd80b150e6a5a4ec9
GUIChef <Professionalrandy@outlook.com>
2026-08-10 17:40:27 -0700

    Digerere addo
    Refactor txtToWav function to improve readability and variable naming.

--- articles/txtToWav.js
+++ articles/txtToWav.js
@@ -1,9 +1,10 @@
 function txtToWav() {
-  const file = document.getElementById("fileUpload").files[0];
-  const reader = new FileReader();
-  reader.readAsText(file);
-  reader.addEventListener('load', (penisPower) => {
-    const data = penisPower.target.result;
-    console.log(data);
+  const file = document.getElementById("fileUpload").files[0];//This gets the file name and stors it to a variable.
+  const reader = new FileReader();//I'm not exactly sure what this does
+  reader.readAsText(file);//This reads the file
+  reader.addEventListener('load', (unpack) => {//something something something
+    const contents = unpack.target.result;//This stores the file in a variable
   })
+  let hex = contents.trim().split(/\s+/);//This creates a new variabe from the contents after seperating by whitespaces
+  console.log(hex);// Remove later
 }
