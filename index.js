fcd28fbd11515ea37a6dd9960dc35ecbcc4a99e4
GUIChef <Professionalrandy@outlook.com>
2026-08-16 21:25:40 -0700

    Claudere addo

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const openMicrowave = new Audio("sounds/openMicrowave.wav");
 
 function microwaveOpen(){
-  document.getElementById("microwave").src="images/microwaveOpen.png"
-  document.getElementById("microwave").title="This is an open microwave."
-  document.getElementById("microwave").alt="This is an open microwave."
-  document.getElementById("microwave").id="microwaveOpen"
-  document.getElementById("microwaveButton").onclick=microwaveClose
+	document.getElementById("microwave").src="images/microwaveOpen.png"
+	document.getElementById("microwave").title="This is an open microwave."
+	document.getElementById("microwave").alt="This is an open microwave."
+	document.getElementById("microwave").id="microwaveOpen"
+	document.getElementById("microwaveButton").onclick=microwaveClose
 }
 function microwaveClose(){
 	document.getElementById("microwaveOpen").src="images/microwave.png"
-  document.getElementById("microwaveOpen").title="This is a microwave."
-  document.getElementById("microwaveOpen").alt="This is a microwave."
-  document.getElementById("microwaveOpen").id="microwave"
-  document.getElementById("microwaveButton").onclick=microwaveOpen
+	document.getElementById("microwaveOpen").title="This is a microwave."
+	document.getElementById("microwaveOpen").alt="This is a microwave."
+	document.getElementById("microwaveOpen").id="microwave"
+	document.getElementById("microwaveButton").onclick=microwaveOpen
 }
 
 function playSound(sound){
-  sound.currentTime=0;
-  sound.play();
+	sound.currentTime=0;
+	sound.play();
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("microwaveButton").addEventListener("click",function(){playSound(openMicrowave);});
+	document.getElementById("microwave").addEventListener("click",function(){playSound(openMicrowave);});
+	document.getElementById("microwaveOpen").addEventListener("click",function(){playSound(openMicrowave);});
 });
