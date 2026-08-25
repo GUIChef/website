0ff4380bbd4ca311ab8c54912caf10e7782c958d
GUIChef <Professionalrandy@outlook.com>
2026-08-24 18:36:04 -0700

    ??!

--- articles/articles.js
+++ articles/articles.js
@@ -1,14 +1,14 @@
-window.addEventListener('load', function () {
-    fetch("articles.json")
-        .then(function(penis) {
+window.addEventListener('load', function () {//only after the page loads
+    fetch("articles.json")//get Jason!
+        .then(function(penis) {//make it js readable
             return penis.json();
         })
-        .then(function(data) {
-            data.forEach(function(item) {
-                if (item.name) {
-                    document.getElementById("menu").innerHTML += "<li>" + item.name + "</li>";
-                } else if (item.link) {
-                    document.getElementById("menu").innerHTML += "<li><a href=\"" + item.link + "\">" + item.link + "</a></li>";
+        .then(function(data) {//what to do once it's been got
+            data.forEach(function(item) {//repeat for every item
+                if (item.children) {//if it contains 'children'
+                    document.getElementById("menu").innerHTML += "<li>" + item.name + "</li>";//only print the top level
+                } else if (item.link) {//or if it contains a link
+                    document.getElementById("menu").innerHTML += "<li><a href=\"" + item.link + "\">" + item.name + "</a></li>";//make 'link' the href and 'name' the shown text.
                 }
             });
         });
