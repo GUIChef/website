fa4ba2e9b9b4982fbd9a09135383f21174e0fde9
GUIChef <Professionalrandy@outlook.com>
2026-08-24 18:54:10 -0700

    Uniquem addo
    Updated menu item rendering to include item ID in list items.

--- articles/articles.js
+++ articles/articles.js
@@ -6,7 +6,7 @@ window.addEventListener('load', function () {//only after the page loads
         .then(function(data) {//what to do once it's been got
             data.forEach(function(item) {//repeat for every item
                 if (item.children) {//if it contains 'children'
-                    document.getElementById("menu").innerHTML += "<li>" + item.name + "</li>";//only print the top level
+                    document.getElementById("menu").innerHTML += "<li\""+item.id+"\">" + item.name + "</li>";//only print the top level
                 } else if (item.link) {//or if it contains a link
                     document.getElementById("menu").innerHTML += "<li><a href=\"" + item.link + "\">" + item.name + "</a></li>";//make 'link' the href and 'name' the shown text.
                 }
