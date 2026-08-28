a110df2f0eba43de984dbc81cef4f03061c6ea92
GUIChef <Professionalrandy@outlook.com>
2026-08-27 17:10:57 -0700

    Adnotationem addo

--- articles/articles.js
+++ articles/articles.js
@@ -13,7 +13,7 @@ window.addEventListener('load', function () {//only after the page loads
             });
             document.getElementById("menu").addEventListener("click", function(vagina) { // detects when anything in the menu is pressed
                 vagina.target.innerHTML+="<ul id=\""+vagina.target.id+"Contents\"></ul>";  //creates an empty unordered list with a similar id  target --> targetContents
-                document.getelementById(vagina.target.id).id = "clit"
+                document.getelementById(vagina.target.id).id = "clit" //prevent it from being clicked again. I <3 clitoris (:
                 var clickedItem = data.find(function(item) { //stores a variable of the clicked item
                     return item.id === vagina.target.id;
                 });
